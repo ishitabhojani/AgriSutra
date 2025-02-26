c634dd3e0935e691482c52644b8c1642d0c48c8a
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2025-02-26 22:12:21 +0530

    schemes files update

--- module3/server/routes/schemes.js
+++ module3/server/routes/schemes.js
@@ -1,18 +1,12 @@
-// server/routes/schemes.js
-
 const express = require("express");
 const { handelGetSchemes, handelGetSchemeById, handleSaveScheme } = require("../controllers/schemesController");
-// const pool = require("../db");
 const authenticateToken = require("../middleware/authMiddleware");
 const router = express.Router();
 
-// GET all schemes
 router.get("/", handelGetSchemes);
 
-// GET a specific scheme by id
 router.get("/:id", handelGetSchemeById);
 
-// POST save scheme (protected route)
 router.post("/:id/save", authenticateToken, handleSaveScheme);
 
 module.exports = router;
